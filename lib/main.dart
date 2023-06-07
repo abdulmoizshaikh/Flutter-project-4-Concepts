6de5099bf30048e9fef30f99ae98379b196a1d99
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-06-07 17:27:46 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:base_app/ui_updates_demo.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const UIUpdatesDemo(),
+      ),
+    );
+  }
+}
